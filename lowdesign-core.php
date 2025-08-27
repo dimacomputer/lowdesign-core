0d74b255a2cc9b51dd65aaab73426efa8f1b735c
Dmitry Litvinov <dimacomputer@gmail.com>
2025-08-27 10:30:42 +0200

    Update lowdesign-core.php

--- lowdesign-core.php
+++ lowdesign-core.php
@@ -4,6 +4,34 @@
  */
 if (!defined('ABSPATH')) { exit; }
 
+error_log('[ld-core] bootstrap start');
+
+add_action('plugins_loaded', static function () {
+    error_log('[ld-core] plugins_loaded enter');
+    $base = __DIR__;
+    $files = [
+        $base . "/inc/acf.php",
+        $base . "/inc/ld_brand_asset.php",
+        $base . "/inc/ld_icon_inline.php",
+        $base . "/inc/get_featured_image_block.php",
+        $base . "/inc/render_svg_inline.php",
+        $base . "/inc/render_featured_svg_inline.php",
+        $base . "/modules/add_excerpt_to_pages.php",
+        $base . "/modules/post_type_config.php",
+        $base . "/modules/taxonomy_ui_role.php",
+    ];
+    foreach ($files as $f) {
+        error_log('[ld-core] try include: '.$f);
+        if (is_readable($f)) {
+            require_once $f;
+            error_log('[ld-core] included: '.$f);
+        } else {
+            error_log('[ld-core] SKIP (not readable): '.$f);
+        }
+    }
+    error_log('[ld-core] plugins_loaded done');
+});
+
 add_action("plugins_loaded", static function () {
     $base = __DIR__;
     $files = [
